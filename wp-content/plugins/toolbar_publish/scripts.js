9fccfefdfed532e7dfcf3063b512acad95efb213
Dalton McGee <dalton.mc@gmail.com>
2022-01-05 23:22:25 -0600

    Added comments

--- wp-content/plugins/toolbar_publish/scripts.js
+++ wp-content/plugins/toolbar_publish/scripts.js
@@ -1,3 +1,11 @@
+/**
+ * The script here basically just finds the User Dropdown Icon,
+ * gets it width, then sets the `right` style attribute on the
+ * publish button to that, then assesses that on any resize so that
+ * no matter the person's displayed name, the button is always in
+ * the correct X position.
+ */
+
 window.addEventListener("DOMContentLoaded", () => {
   const userButton = document.getElementById('wp-admin-bar-top-secondary');
   const publishButton = document.getElementById('wp-admin-bar-publish');
