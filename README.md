e081c185c4b7a0bb2da79e25a65cdb0e502a3652
Dalton McGee <dalton.mc@gmail.com>
2022-01-05 18:02:19 -0600

    add in wordpress boilerplate

---
+++ README.md
@@ -0,0 +1,7 @@
+# 2022-12-03
+
+### <a href="#" target="_blank">Live Demo</a>
+
+### Technologies
+
+### Resources and details
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+
+ * @package Sticky Publish
+
+ */
+
+/*
+
+Plugin Name: Sticky Publish
+
+Description: Make the publish modal on the right-hand side of Wordpress stay with you as you scroll.
+
+Version: 1.0.0
+
+Author: Dalton McGee
+
+License: MIT
+
+*/
