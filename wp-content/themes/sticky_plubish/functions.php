067d397a0359a30b296dddae48203c5bbf4214ce
Dalton McGee <dalton.mc@gmail.com>
2022-01-05 22:20:12 -0600

    Added a bunch of comments

--- wp-content/themes/sticky_plubish/functions.php
+++ wp-content/themes/sticky_plubish/functions.php
@@ -2,8 +2,3 @@
 
 $dir = __DIR__;
 require_once("$dir/vendor/autoload.php");
-require_once("$dir/loaders.php");
-
-Loaders::requireAllFilesinDir("$dir/models/blocks");
-Loaders::requireAllFilesinDir("$dir/models/post_types");
-
